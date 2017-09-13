acfee1810b567792df401c11bb76f53bb9db33cd
chaboom <30657926+chaboom@users.noreply.github.com>
2017-09-13 15:30:50 +0900

    Create 13458.cpp
    answer 범위초과가 관건

---
+++ 13458.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{ 
+    int N, temp; 
+    vector<int> A; 
+    int B, C; 
+    unsigned long long int ans = 0;
+    cin >> N; 
+   for (int i = 0; i < N; i++) 
+   {  
+    cin >> temp;  A.push_back(temp); 
+   } 
+   cin >> B >> C;
+
+   for (int i = 0; i < N; i++)
+   {  
+     if (A[i] != 0) ans++; 
+     A[i] = A[i] - B;  
+     if (A[i] <= 0) A[i] = -1;  
+     if ((A[i] / C) >= 1) ans += A[i] / C;  
+     if ((A[i] % C) > 0) ans++; 
+   }
+     A.clear();
+     cout << ans;
+     return 0;
+ }
